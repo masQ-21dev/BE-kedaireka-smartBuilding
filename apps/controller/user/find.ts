dc1dc12d9efb842c6d0d8849c8e7c1f98db31791
Josephsaru <josephsarumaha72@gmail.com>
2023-09-25 15:41:25 +0700

    setuju

--- apps/controller/user/find.ts
+++ apps/controller/user/find.ts
@@ -1,7 +1,7 @@
 import { type Response } from 'express'
 
 export const findAllUser = async function (req: any, res: Response): Promise<any> {
-  return res.send('ok')
+  return res.send('setuju')
 }
 
 export const findOneUserByUserId = async function (req: any, res: Response): Promise<any> {
